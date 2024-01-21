c8348918c2e495e35a4aa190b501e1c604c7ff83
JethroNatividad <natividad.jet@gmail.com>
2024-01-21 11:57:10 +0800

    implement send message from file

--- main.py
+++ main.py
@@ -2,29 +2,31 @@ import aiohttp
 import asyncio
 import time
 import uuid
+import random
 
 # https://ngl.link/api/submit
 times = 10
 
-async def send_message(username, question):
+async def send_message(username, questions):
     try:
         async with aiohttp.ClientSession() as session:
             random_device_id = uuid.uuid4()
+            random_question = random.choice(questions)
             data = {
                 "username": username,
-                "question": question,
+                "question": random_question,
                 "deviceId": random_device_id
             }
             async with session.post(url="https://ngl.link/api/submit", data=data):
-                print(f"Sent {question} to @{username} as {random_device_id}")
+                print(f"Sent {random_question} to @{username} as {random_device_id}")
     except Exception as e:
         print(e)
 
 async def main():
-    username = "cottonfarmer112"
-    question = "cottonfarmer112"
-    tasks = [send_message(username, question) for _ in range(times)]
-    await asyncio.gather(*tasks)
+        badwords = [line.strip() for line in open('bad.txt', 'r')]
+        username = "cottonfarmer112"
+        tasks = [send_message(username, badwords) for _ in range(times)]
+        await asyncio.gather(*tasks)
 
 start = time.time()
 asyncio.run(main())
