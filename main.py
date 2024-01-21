79628d902a221c7b7c0f7398d4539a859796e7da
JethroNatividad <natividad.jet@gmail.com>
2024-01-21 11:43:27 +0800

    add uuid

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import aiohttp
 import asyncio
 import time
+import uuid
+
 # https://ngl.link/api/submit
 
 username = "cottonfarmer112"
 question = "cottonfarmer112"
 
-data = {
-    "username": username,
-    "question": question,
-    "deviceId": "e6baa765-afad-4885-a06f-13b5d54d7ce7"
-}
+
 
 async def request():
     try:
         async with aiohttp.ClientSession() as session:
+            data = {
+                "username": username,
+                "question": question,
+                "deviceId": uuid.uuid4()
+            }
             async with session.post(url="https://ngl.link/api/submit", data=data):
                 print("sent")
     except Exception as e:
