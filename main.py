cd618925baa3336d775da403a0213e6eff3aa0e8
JethroNatividad <natividad.jet@gmail.com>
2024-01-21 17:51:28 +0800

    show erro messages

--- main.py
+++ main.py
@@ -16,29 +16,36 @@ async def send_message(username, questions):
                 "question": random_question,
                 "deviceId": random_device_id
             }
-            async with session.post(url="https://ngl.link/api/submit", data=data):
-                print(f"Sent '{random_question}' to @{username}.                               ", end="\r")
+            async with session.post(url="https://ngl.link/api/submit", data=data) as response:
+                print("Status:", response.status)
+                if response.status != 200:
+                    print(f"Error sending '{random_question}' to @{username}.")
+                else:
+                    print(f"Sent '{random_question}' to @{username}.")
+                return response.status
     except Exception as e:
         print(e)
 
 
 async def spam(username, count):
         messages = [line.strip() for line in open('messages.txt', 'r')]
-        tasks = [send_message(username, messages) for _ in range(count)]
-        await asyncio.gather(*tasks)
 
+        start = time.time() 
+        tasks = [send_message(username, messages) for _ in range(count)]
+        results = await asyncio.gather(*tasks)
+        end = time.time()
+
+        # Count the number of success status
+        success = results.count(200)
+        seconds = end - start
+        print(f"\nTook {seconds} seconds to send {success} messages.")
+        
 @click.command()
 @click.option('--username', prompt='NGL Username',
               help='The username to spam.')
 @click.option('--count', prompt='Spam count', type=int, help='n times to spam.')
 def main(username, count):
-
-    start = time.time()
     asyncio.run(spam(username, count))
-    end = time.time()
-
-    seconds = end - start
-    print(f"\nTook {seconds} seconds to send {count} messages.")
 
 f = Figlet(font='slant')
 print(f.renderText('NGL SPAMMER'))
