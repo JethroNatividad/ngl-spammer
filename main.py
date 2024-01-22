38c4e464f08705abfb5eab2fb4f88e0e6b2fd006
JethroNatividad <natividad.jet@gmail.com>
2024-01-22 15:51:32 +0800

    Add headers to make it work every requests

--- main.py
+++ main.py
@@ -16,7 +16,26 @@ async def send_message(username, questions):
                 "question": random_question,
                 "deviceId": random_device_id
             }
-            async with session.post(url="https://ngl.link/api/submit", data=data) as response:
+
+            headers = {
+                'Host': 'ngl.link',
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0',
+                'Accept': '*/*',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'X-Requested-With': 'XMLHttpRequest',
+                'Origin': 'https://ngl.link',
+                'Connection': 'keep-alive',
+                'Referer': 'https://ngl.link/cottonfarmer112',
+                'Cookie': 'cf_clearance=9.S5CVqUSjqzonDp6r1lI2.WRljn9mg9AbMWsPndK1A-1705909134-1-Afelaz2NWnJrlH4sS34Ng+lVlBtpuiUXcXO3NIPkPa9vgFcvitt8JelNSZDuNqU3kpjU3COPvRRIFuIxDGMv9MU=; __stripe_mid=6c1b9323-6781-4cd6-9574-e21b1755d270a14a35; __stripe_sid=984f4f78-81de-4306-a6d6-88b38ecc15cf1ca9d5',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'TE': 'trailers',
+            }
+            
+            async with session.post(url="https://ngl.link/api/submit", data=data, headers=headers) as response:
                 print("Status:", response.status)
                 if response.status != 200:
                     print(f"Error sending '{random_question}' to @{username}.")
